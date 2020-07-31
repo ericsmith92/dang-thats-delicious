d0f3fb422ed4e753f71a5aa2de0ed59b5eb7e0aa
ericsmith92 <ericsmth884@gmail.com>
2020-07-31 08:45:00 -0400

    adds logic to increment non-unique slugs so they are unique

--- models/Store.js
+++ models/Store.js
@@ -37,13 +37,21 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
     if(!this.isModified('name')){
         next(); //skip it
         return; //stop function from running
     }
 
     this.slug = slug(this.name);
+    //find other stores that have slug of start-of-string
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    //see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor for more on this.constructor
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if(storesWithSlug.length){
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
 
     //TODO: Add logic for unique slugs
